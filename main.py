b0622f7e4b840c162fb1b6f2590d123d59a9f201
ashvaykovskyas-del <a.shvaykovsky.as@gmail.com>
2026-03-02 13:46:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from fastapi import FastAPI
+import os
 
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "tg-parser is running"}
+
+@app.get("/health")
+def health():
+    return {"health": "ok"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
